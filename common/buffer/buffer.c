68aa059228304572526bbab44ff80eae6bdaccb2
Hyalunar <90334612+Hyalunar@users.noreply.github.com>
2023-05-11 19:02:15 +0200

    Update buffer.c
    Comply to variable naming convention

--- common/buffer/buffer.c
+++ common/buffer/buffer.c
@@ -7,41 +7,41 @@ buffer_t* CreateBuffer(size_t size)
     buffer_t* buffer;
     
     buffer = (buffer_t*) malloc(sizeof(buffer_t));
-    buffer->data_start = (char*)malloc(size);
-    buffer->allocated_length = size;
-    buffer->data_end = buffer->data_start;
+    buffer->dataStart = (char*)malloc(size);
+    buffer->allocatedLength = size;
+    buffer->dataEnd = buffer->dataStart;
     return buffer;
 }
 
 void WriteBuffer(buffer_t* buffer, void* data, size_t len)
 {
     char* bytes = (char*) data;
-    if(len + GetBufferSize(buffer) > buffer->allocated_length) {
+    if(len + GetBufferSize(buffer) > buffer->allocatedLength) {
         printf("ERROR: Buffer Overflow! Please resize! (If you can find out how)\n");
         return;
     }
     
     for(int i = 0; i < len; i++) {
-        buffer->data_end[0] = bytes[i];
-        buffer->data_end++;
+        buffer->dataEnd[0] = bytes[i];
+        buffer->dataEnd++;
     }
     return;
 }
 
 void EraseBuffer(buffer_t* buffer)
 {
-    buffer->data_end = buffer->data_start;
+    buffer->dataEnd = buffer->dataStart;
     return;
 }
 
 size_t GetBufferSize(buffer_t* buffer)
 {
-    return buffer->data_end - buffer->data_start;
+    return buffer->dataEnd - buffer->dataStart;
 }
 
 void DeleteBuffer(buffer_t* buffer)
 {
-    free(buffer->data_start);
+    free(buffer->dataStart);
     free(buffer);
     return;
 }
