c723b7b7e09f732e2a65136d27dc3ed875611d86
Hyalunar <vegotenksGogeto@gmx.de>
2023-05-11 15:07:36 +0200

    Refactored variables for readability

--- common/buffer/buffer.c
+++ common/buffer/buffer.c
@@ -6,42 +6,42 @@ buffer_t* CreateBuffer(size_t size)
 {
     buffer_t* buffer;
     
-    buffer = (buffer_t*)malloc(sizeof(buffer_t));
-    buffer->data = (char*)malloc(size);
-    buffer->totalSize = size;
-    buffer->ptr = buffer->data;
+    buffer = (buffer_t*) malloc(sizeof(buffer_t));
+    buffer->data_start = (char*)malloc(size);
+    buffer->allocated_length = size;
+    buffer->data_end = buffer->data_start;
     return buffer;
 }
 
 void WriteBuffer(buffer_t* buffer, void* data, size_t len)
 {
     char* bytes = (char*) data;
-    if(len + GetBufferSize(buffer) > buffer->totalSize) {
+    if(len + GetBufferSize(buffer) > buffer->allocated_length) {
         printf("ERROR: Buffer Overflow! Please resize! (If you can find out how)\n");
         return;
     }
     
     for(int i = 0; i < len; i++) {
-        buffer->ptr[0] = bytes[i];
-        buffer->ptr++;
+        buffer->data_end[0] = bytes[i];
+        buffer->data_end++;
     }
     return;
 }
 
-void ResetBuffer(buffer_t* buffer)
+void EraseBuffer(buffer_t* buffer)
 {
-    buffer->ptr = buffer->data;
+    buffer->data_end = buffer->data_start;
     return;
 }
 
 size_t GetBufferSize(buffer_t* buffer)
 {
-    return buffer->ptr - buffer->data;
+    return buffer->data_end - buffer->data_start;
 }
 
 void DeleteBuffer(buffer_t* buffer)
 {
-    free(buffer->data);
+    free(buffer->data_start);
     free(buffer);
     return;
 }
